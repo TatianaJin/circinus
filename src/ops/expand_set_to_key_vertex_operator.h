ab099e7ee0ffe327475fe62c6f9d7732dae73c1a
Tatiana Jin <tatiana.jin.xy@gmail.com>
2021-03-18 22:57:00 +0800

    [OP] adaptive strategy for expand setTokey OP  (#13)
    Co-authored-by: curiosityyy <24curiosity@gmail.com>

--- src/ops/expand_set_to_key_vertex_operator.h
+++ src/ops/expand_set_to_key_vertex_operator.h
@@ -15,11 +15,13 @@
 #pragma once
 
 #include <string>
-#include <unordered_set>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 #include "graph/query_graph.h"
 #include "ops/expand_vertex_operator.h"
+#include "utils/hashmap.h"
 
 namespace circinus {
 
@@ -29,42 +31,17 @@ class ExpandSetToKeyVertexOperator : public ExpandVertexOperator {
                                const std::unordered_map<QueryVertexID, uint32_t>& query_vertex_indices)
       : ExpandVertexOperator(parents, target_vertex, query_vertex_indices) {}
 
+  // TODO(tatiana): see if hard limit on output size is needed
   uint32_t expand(std::vector<CompressedSubgraphs>* outputs, uint32_t batch_size) override {
     uint32_t output_num = 0;
     while (input_index_ < current_inputs_->size()) {
-      std::unordered_set<VertexID> visited;
-      auto& input = (*current_inputs_)[input_index_];
-      const auto& parent_match = input.getSet(query_vertex_indices_[parents_[0]]);
-      for (VertexID vid : *parent_match) {
-        const auto& out_neighbors = current_data_graph_->getOutNeighbors(vid);
-        for (uint32_t i = 0; i < out_neighbors.second; ++i) {
-          VertexID key_vertex_id = out_neighbors.first[i];
-          if (visited.find(key_vertex_id) == visited.end()) {
-            visited.insert(key_vertex_id);
-            if (!isInCandidates(key_vertex_id)) {
-              continue;
-            }
-            auto key_out_neighbors = current_data_graph_->getOutNeighbors(key_vertex_id);
-            // TODO(by) hash key_out_neighbors
-            CompressedSubgraphs new_output(input, key_vertex_id);
-            bool add = true;
-            for (uint32_t set_vid : parents_) {
-              std::vector<VertexID> new_set;
-              uint32_t id = query_vertex_indices_[set_vid];
-              intersect(*input.getSet(id), key_out_neighbors, &new_set);
-              if (new_set.size() == 0) {
-                add = false;
-                break;
-              }
-              new_output.UpdateSets(id, std::make_shared<std::vector<VertexID>>(std::move(new_set)));
-            }
-
-            if (add) {
-              outputs->emplace_back(std::move(new_output));
-              output_num++;
-            }
-          }
-        }
+      auto min_parent_set = getMinimumParent();
+      if (min_parent_set.first * 20 < candidates_->size()) {
+        DLOG(INFO) << "fromSetNeighborStrategy";
+        output_num += fromSetNeighborStrategy(outputs, min_parent_set.second);
+      } else {
+        DLOG(INFO) << "fromCandidateStrategy";
+        output_num += fromCandidateStrategy(outputs);
       }
       input_index_++;
       if (output_num >= batch_size) {
@@ -89,9 +66,105 @@ class ExpandSetToKeyVertexOperator : public ExpandVertexOperator {
   }
 
  protected:
+  uint32_t profile() {
+    auto& input = (*current_inputs_)[input_index_];
+    LOG(INFO) << "---------------------";
+    for (auto par : parents_) {
+      uint32_t loop_num = 0;
+      const auto& parent_match = input.getSet(query_vertex_indices_[par]);
+      for (VertexID vid : *parent_match) {
+        const auto& out_neighbors = current_data_graph_->getOutNeighbors(vid);
+        for (uint32_t i = 0; i < out_neighbors.second; ++i) {
+          loop_num++;
+        }
+      }
+      LOG(INFO) << "set out_neighbors num " << loop_num;
+    }
+    LOG(INFO) << "candidates num " << candidates_->size();
+    return 0;
+  }
+
   bool isInCandidates(VertexID key) {
     auto lb = std::lower_bound(candidates_->begin(), candidates_->end(), key);
     return lb != candidates_->end() && *lb == key;
+  }
+
+  std::pair<uint32_t, uint32_t> getMinimumParent() {
+    uint32_t parent = 0, size = 0xFFFFFFFF;
+    auto& input = (*current_inputs_)[input_index_];
+    for (auto par : parents_) {
+      auto current_size = input.getSet(query_vertex_indices_[par])->size();
+      if (current_size < size) {
+        size = current_size;
+        parent = par;
+      }
+    }
+    return std::make_pair(size, parent);
+  }
+
+  uint32_t fromCandidateStrategy(std::vector<CompressedSubgraphs>* outputs) {
+    auto& input = (*current_inputs_)[input_index_];
+    uint32_t output_num = 0;
+    for (VertexID key_vertex_id : *candidates_) {
+      auto key_out_neighbors = current_data_graph_->getOutNeighbors(key_vertex_id);
+      // TODO(by) hash key_out_neighbors
+      CompressedSubgraphs new_output(input, key_vertex_id);
+      bool add = true;
+      for (uint32_t set_vid : parents_) {
+        std::vector<VertexID> new_set;
+        uint32_t id = query_vertex_indices_[set_vid];
+        intersect(*input.getSet(id), key_out_neighbors, &new_set);
+        if (new_set.empty()) {
+          add = false;
+          break;
+        }
+        new_output.UpdateSets(id, std::make_shared<std::vector<VertexID>>(std::move(new_set)));
+      }
+
+      if (add) {
+        outputs->emplace_back(std::move(new_output));
+        output_num++;
+      }
+    }
+    return output_num;
+  }
+
+  uint32_t fromSetNeighborStrategy(std::vector<CompressedSubgraphs>* outputs, QueryVertexID min_parent) {
+    unordered_set<VertexID> visited;
+    auto& input = (*current_inputs_)[input_index_];
+    uint32_t output_num = 0;
+    const auto& parent_match = input.getSet(query_vertex_indices_[min_parent]);
+    for (VertexID vid : *parent_match) {
+      const auto& out_neighbors = current_data_graph_->getOutNeighbors(vid);
+      for (uint32_t i = 0; i < out_neighbors.second; ++i) {
+        VertexID key_vertex_id = out_neighbors.first[i];
+        if (visited.insert(key_vertex_id).second) {
+          if (!isInCandidates(key_vertex_id)) {
+            continue;
+          }
+          auto key_out_neighbors = current_data_graph_->getOutNeighbors(key_vertex_id);
+          // TODO(by) hash key_out_neighbors
+          CompressedSubgraphs new_output(input, key_vertex_id);
+          bool add = true;
+          for (uint32_t set_vid : parents_) {
+            std::vector<VertexID> new_set;
+            uint32_t id = query_vertex_indices_[set_vid];
+            intersect(*input.getSet(id), key_out_neighbors, &new_set);
+            if (new_set.empty()) {
+              add = false;
+              break;
+            }
+            new_output.UpdateSets(id, std::make_shared<std::vector<VertexID>>(std::move(new_set)));
+          }
+
+          if (add) {
+            outputs->emplace_back(std::move(new_output));
+            output_num++;
+          }
+        }
+      }
+    }
+    return output_num;
   }
 };
 
