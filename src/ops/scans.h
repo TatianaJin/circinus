72156ae9754e7884fdd51ed92e22f60b41f7a9e5
Tatiana Jin <tatiana.jin.xy@gmail.com>
2021-05-20 20:14:21 +0800

    [Op] Physical scan operators

--- src/ops/scans.h
+++ src/ops/scans.h
@@ -14,4 +14,55 @@
 
 #pragma once
 
-#include "ops/scans/ldf_scan.h"
+#include <numeric>
+#include <vector>
+
+#include "exec/execution_config.h"
+#include "graph/graph.h"
+#include "graph/query_graph.h"
+#include "graph/types.h"
+
+namespace circinus {
+
+struct ScanContext {
+  std::vector<VertexID> candidates;  // output
+  VertexID scan_offset = -1;
+  VertexID scan_end = 0;
+
+  ScanContext(VertexID offset, VertexID end) : scan_offset(offset), scan_end(end) {}
+};
+
+/**
+ * The Scan operator should be functor-like so as to be shared by multiple threads to run parallel tasks.
+ */
+class Scan {
+ private:
+  uint32_t parallelism_ = 1;
+  VertexID scan_size_ = 0;
+
+ public:
+  /**
+   * @param label_pruning_method The method to filter vertices by label: 2 for finding label range, 1 for building label
+   * index, 0 for full scan.
+   */
+  static Scan* newLDFScan(LabelID label, VertexID out_d, VertexID in_d, ExecutionConfig& conf,
+                          uint32_t label_pruning_method);
+  static Scan* newDegreeScan(VertexID out_d, VertexID in_d, ExecutionConfig& conf);
+
+  Scan() {}
+  explicit Scan(ExecutionConfig& conf) : parallelism_(conf.getMaxParallelism()), scan_size_(conf.getInputSize()) {}
+
+  inline ScanContext initScanContext(uint32_t task_idx) {
+    DCHECK_LT(task_idx, parallelism_);
+    auto chunk_size = scan_size_ / task_idx;
+    CHECK_NE(chunk_size, 0);
+    if (task_idx < scan_size_ % task_idx) {
+      return ScanContext((chunk_size + 1) * task_idx, chunk_size + 1);
+    }
+    return ScanContext(chunk_size * task_idx + (scan_size_ % task_idx), chunk_size);
+  }
+
+  virtual void scan(const Graph* g, uint32_t batch_size, ScanContext* ctx) const = 0;
+};
+
+}  // namespace circinus
