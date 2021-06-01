c1ab4c97374dd387a598b5bb2e594cf8b20eada3
qlma <b3ng1998@gmail.com>
2021-06-01 19:07:53 +0800

    format

--- scripts/duplication_checker.sh
+++ scripts/duplication_checker.sh
@@ -6,29 +6,29 @@ logdir="${3:-/home/qlma/log/}"
 let cnt=0
 for dir1 in $path/*; do
   for dir2 in $path/*; do
-		file1=$(basename $dir1)
-		file2=$(basename $dir2)
-		if [ $file1 = $file2 ]
-		then
-			break
-		else
-			prefix1=${file1%_*}
-			prefix2=${file2%_*}
-			if [ $prefix1 = $prefix2 ]
-			then
-				$checker -naive_run=1 -naive_datagraph=$dir1 -naive_querygraph=$dir2 -log_dir=$logdir | grep 'MATCH!!!'
-				if [ $? -eq 0 ]
-				then
-					echo $dir1
-					break
-				fi
-			fi
-		fi
+    file1=$(basename $dir1)
+    file2=$(basename $dir2)
+    if [ $file1 = $file2 ]
+    then
+      break
+    else
+      prefix1=${file1%_*}
+      prefix2=${file2%_*}
+      if [ $prefix1 = $prefix2 ]
+      then
+        $checker -naive_run=1 -naive_datagraph=$dir1 -naive_querygraph=$dir2 -log_dir=$logdir | grep 'MATCH!!!'
+        if [ $? -eq 0 ]
+        then
+          echo $dir1
+          break
+        fi
+      fi
+    fi
   done
-	if [ $((cnt%10)) -eq 0 ]
-	then
-		echo $cnt
-	fi
-	let cnt=cnt+1
+  if [ $((cnt%10)) -eq 0 ]
+  then
+    echo $cnt
+  fi
+  let cnt=cnt+1
 done
 
