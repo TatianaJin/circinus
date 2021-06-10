ade154eb0d914bcfbc5ac148c61364e11d10118a
qlma <b3ng1998@gmail.com>
2021-06-10 16:56:00 +0800

    format

--- tests/test_filter_candidates.cc
+++ tests/test_filter_candidates.cc
@@ -86,56 +86,57 @@ const std::vector<int> query_size_list = {4, 8, 12, 16, 20, 24, 32};
 const std::vector<std::string> query_mode_list = {"dense", "sparse"};
 const std::pair<int, int> query_index_range = {1, 200};
 
-std::vector<std::vector<VertexID>> getCandidateSets(const Graph& g, const QueryGraph& q, const std::string& filter_str) {
-    std::vector<std::vector<VertexID>> candidates(q.getNumVertices());
-    std::vector<VertexID> candidate_size(q.getNumVertices());
-    ExecutionConfig config;
-    for (uint32_t v = 0; v < q.getNumVertices(); ++v) {
-      config.setInputSize(g.getVertexCardinalityByLabel(q.getVertexLabel(v)));
-      auto scan = circinus::Scan::newLDFScan(q.getVertexLabel(v), q.getVertexOutDegree(v), 0, config, 1);
-      if (filter_str.compare("ldf")&& filter_str.compare("dpiso") ) {
-        scan->addFilter(std::make_unique<NLFFilter>(&q, v));
-      }
-      auto scan_ctx = scan->initScanContext(0);
-      scan->scan(&g, &scan_ctx);
-      candidates[v] = std::move(scan_ctx.candidates);
-      candidate_size[v] = candidates[v].size();
-      //LOG(INFO) << "query vertex " << v << ' ' << scan->toString();
+std::vector<std::vector<VertexID>> getCandidateSets(const Graph& g, const QueryGraph& q,
+                                                    const std::string& filter_str) {
+  std::vector<std::vector<VertexID>> candidates(q.getNumVertices());
+  std::vector<VertexID> candidate_size(q.getNumVertices());
+  ExecutionConfig config;
+  for (uint32_t v = 0; v < q.getNumVertices(); ++v) {
+    config.setInputSize(g.getVertexCardinalityByLabel(q.getVertexLabel(v)));
+    auto scan = circinus::Scan::newLDFScan(q.getVertexLabel(v), q.getVertexOutDegree(v), 0, config, 1);
+    if (filter_str.compare("ldf") && filter_str.compare("dpiso")) {
+      scan->addFilter(std::make_unique<NLFFilter>(&q, v));
     }
-
-    if (filter_str.compare("ldf") && filter_str.compare( "nlf")) {
-      auto metadata = GraphMetadata(g);
-      std::unique_ptr<LogicalNeighborhoodFilter> logical_filter;
-      if (filter_str.compare("cfl")) {
-        logical_filter = std::make_unique<LogicalCFLFilter>(metadata, &q, candidate_size);
-      } else if (filter_str.compare( "dpiso")) {
-        logical_filter = std::make_unique<LogicalDPISOFilter>(metadata, &q, candidate_size);
-      } else if (filter_str.compare( "tso")) {
-        logical_filter = std::make_unique<LogicalTSOFilter>(metadata, &q, candidate_size);
-      } else if (filter_str.compare( "gql")) {
-        logical_filter = std::make_unique<LogicalGQLFilter>(&q);
-      }
-      auto physical_filters = logical_filter->toPhysicalOperators(metadata, config);
-   // LOG(INFO) << "total number of physical filters: " << physical_filters.size() << '\n';
-      for (auto& filter : physical_filters) {
-        QueryVertexID query_vertex = filter->getQueryVertex();
-        filter->setInputSize(candidate_size[query_vertex]);
-        auto filter_ctx = filter->initFilterContext(0);
-        filter->filter(&g, &candidates, &filter_ctx);
-        candidates[query_vertex].erase(std::remove_if(candidates[query_vertex].begin(), candidates[query_vertex].end(),
-                                                      [invalid_vertex_id = INVALID_VERTEX_ID](VertexID & candidate) {
-                                                        return candidate == invalid_vertex_id;
-                                                      }),
-                                       candidates[query_vertex].end());
-        candidate_size[query_vertex] = candidates[query_vertex].size();
-        // LOG(INFO) << query_vertex << " -------- " << candidate_size[query_vertex];
-      }
-      // for (QueryVertexID i = 0; i < q.getNumVertices(); ++i) {
-      //   LOG(INFO) << "query vertex " << i << " candidate size: " << candidates[i].size() << '\n';
-      // }
-    }
-    return candidates;
+    auto scan_ctx = scan->initScanContext(0);
+    scan->scan(&g, &scan_ctx);
+    candidates[v] = std::move(scan_ctx.candidates);
+    candidate_size[v] = candidates[v].size();
+    // LOG(INFO) << "query vertex " << v << ' ' << scan->toString();
   }
+
+  if (filter_str.compare("ldf") && filter_str.compare("nlf")) {
+    auto metadata = GraphMetadata(g);
+    std::unique_ptr<LogicalNeighborhoodFilter> logical_filter;
+    if (filter_str.compare("cfl")) {
+      logical_filter = std::make_unique<LogicalCFLFilter>(metadata, &q, candidate_size);
+    } else if (filter_str.compare("dpiso")) {
+      logical_filter = std::make_unique<LogicalDPISOFilter>(metadata, &q, candidate_size);
+    } else if (filter_str.compare("tso")) {
+      logical_filter = std::make_unique<LogicalTSOFilter>(metadata, &q, candidate_size);
+    } else if (filter_str.compare("gql")) {
+      logical_filter = std::make_unique<LogicalGQLFilter>(&q);
+    }
+    auto physical_filters = logical_filter->toPhysicalOperators(metadata, config);
+    // LOG(INFO) << "total number of physical filters: " << physical_filters.size() << '\n';
+    for (auto& filter : physical_filters) {
+      QueryVertexID query_vertex = filter->getQueryVertex();
+      filter->setInputSize(candidate_size[query_vertex]);
+      auto filter_ctx = filter->initFilterContext(0);
+      filter->filter(&g, &candidates, &filter_ctx);
+      candidates[query_vertex].erase(std::remove_if(candidates[query_vertex].begin(), candidates[query_vertex].end(),
+                                                    [invalid_vertex_id = INVALID_VERTEX_ID](VertexID & candidate) {
+                                                      return candidate == invalid_vertex_id;
+                                                    }),
+                                     candidates[query_vertex].end());
+      candidate_size[query_vertex] = candidates[query_vertex].size();
+      // LOG(INFO) << query_vertex << " -------- " << candidate_size[query_vertex];
+    }
+    // for (QueryVertexID i = 0; i < q.getNumVertices(); ++i) {
+    //   LOG(INFO) << "query vertex " << i << " candidate size: " << candidates[i].size() << '\n';
+    // }
+  }
+  return candidates;
+}
 
 void run(const std::string& dataset, const std::string& filter, std::vector<std::string>& answers) {
   auto graph_path = dataset + "/data_graph/" + dataset + ".graph";
