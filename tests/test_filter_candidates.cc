5ecf3d32ab9e1ed19c63cc82af3e703a4c140983
beng <beng1998@yeah.net>
2021-06-20 20:26:47 +0800

    fix

--- tests/test_filter_candidates.cc
+++ tests/test_filter_candidates.cc
@@ -106,7 +106,7 @@ void run(const std::string& dataset, const std::string& filter, std::vector<std:
         QueryGraph q(query_dir);  // load query graph
         std::stringstream ss;
         ss << dataset << ',' << query_size << ',' << query_mode << ',' << i << ':';
-        FilterAndOrder fao(g, q,filter);
+        FilterAndOrder fao(&g, &q,filter);
         auto candidates = fao.getCandidateSets();  // get candidates for each query vertex
         for (auto v : candidates) {
           ss << v.size() << ' ';
